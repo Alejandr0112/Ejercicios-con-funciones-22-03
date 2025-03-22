bc38985504a98277d727056170e6b1894755e4df
Alejandr0112 <erickalejandro0112@gmail.com>
2025-03-22 10:46:36 -0600

    Potencia

--- main.cpp
+++ main.cpp
@@ -32,3 +32,13 @@ void dividir() {
         cout << "Division: " << (a / b) << endl;
     }
 }
+
+void potencia() {
+    int base, exp, resultado = 1;
+    cout << "Ingrese base y exponente: ";
+    cin >> base >> exp;
+    for(int i = 0; i < exp; i++) {
+        resultado *= base;
+    }
+    cout << "Potencia: " << resultado << endl;
+}
