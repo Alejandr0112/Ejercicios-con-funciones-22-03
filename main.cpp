20fda007baa841136d35c6879d36b7b842f3e6dd
Alejandr0112 <erickalejandro0112@gmail.com>
2025-03-22 10:44:32 -0600

    Multiplicar

--- main.cpp
+++ main.cpp
@@ -14,3 +14,10 @@ void restar() {
     cin >> a >> b;
     cout << "Resta: " << (a - b) << endl;
 }
+
+void multiplicar() {
+    int a, b;
+    cout << "Ingrese dos numeros: ";
+    cin >> a >> b;
+    cout << "Multiplicacion: " << (a * b) << endl;
+}
